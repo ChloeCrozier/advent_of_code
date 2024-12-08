48b0d9880f1cdeb41a4ace116ff7845fbaa69b97
ChloeCrozier <chloecrozier@gmail.com>
2024-12-08 01:05:54 -0500

    day1

--- 2024/day1.py
+++ 2024/day1.py
@@ -1,3 +1,38 @@
-data = fopen("day1.txt", "r")
-lines = data.readlines()
-print(lines)
+import sys
+data = (open(sys.argv[1], 'r')).readlines()
+
+left = []
+right = []
+
+for line in data:
+    vals = line.split('   ')
+    left.append(int(vals[0]))
+    right.append(int(vals[1][0:-1]))
+
+left.sort()
+right.sort()
+totalDist = 0
+
+for i in range(len(left)):
+    totalDist += max(left[i], right[i]) - min(left[i], right[i])
+
+# Part 1 -------
+print(totalDist)
+# --------------
+
+rightMap = {}
+for num in right:
+    if num in rightMap:
+        rightMap[num] += 1
+    else:
+        rightMap[num] = 1
+
+similarity = 0
+
+for num in left:
+    if num in rightMap:
+        similarity += num * rightMap[num]
+
+# Part 1 --------
+print(similarity)
+# ---------------
--- 2024/get_input_data.py
+++ 2024/get_input_data.py
@@ -1,19 +1,20 @@
+# Run with: python get_input_data.py {DAY}
 from dotenv import load_dotenv
 import requests
 import sys
 import os
-
-load_dotenv(dotenv_path='../.env', verbose=True)
-session_id = os.getenv('AOC_SESSION_ID')
 
 if len(sys.argv) < 2:
     print('Usage: python get_input_data.py AOC {DAY}')
     sys.exit(1)
 else:
     url = f'https://adventofcode.com/2024/day/{sys.argv[1]}/input'
+
+    load_dotenv(dotenv_path='../.env', verbose=True)
+    session_id = os.getenv('AOC_SESSION_ID')
     headers = {'Cookie': f'session={session_id}'}
+
     page = requests.get(url, headers=headers)
-    print(page.text)
 
     input_file = open(f'input/day{sys.argv[1]}.txt', 'w')
     input_file.write(page.text)
